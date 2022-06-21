30f920ea05de4d27bb015cd4295b7dcbe0cc9c18
developer-naveen <developer.navi@gmail.com>
2022-06-21 11:44:55 +0530

    getupdate

--- src/index.js
+++ src/index.js
@@ -8,7 +8,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+onst monngodb_url="mongodb+srv://naveen-developer:rash47rash2021@naveen-developer.eja8d.mongodb.net/project-blog";
+
+mongoose.connect(monngodb_url, {useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+    console.log("MongoDB is connected");
+}).catch((error)=>{
+    console.log("Sorry! MongoDB is not connected");
+    console.log(error);
+});
+
+
+
 app.use('/', route);
+
+
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
